a2d24c8b21c2c0bda575583ff5a60ff209929d4f
Vignesh <svignesh1123@gmail.com>
2022-08-23 09:00:41 +0530

    updated by locator for forgot password field

--- src/main/java/com/qa/opencart/pages/LoginPage.java
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -15,7 +15,7 @@ public class LoginPage {
 	private By emailId = By.id("input-email");
 	private By password = By.id("input-password");
 	private By loginBtn = By.xpath("//input[@value='Login']");
-	private By forgotPwdLink = By.linkText("Forgotten Password11");
+	private By forgotPwdLink = By.linkText("Forgotten Password");
 
 	private By registerLink = By.linkText("Register");
 
